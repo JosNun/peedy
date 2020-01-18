94b5345a05ce7969b3277781d916b20c49cc488a
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-01-18 12:08:54 -0500

    Add test for when not passed any args

--- app.test.ts
+++ app.test.ts
@@ -13,4 +13,12 @@ describe('Test rendering a PDF from a url', () => {
     expect(response.status).toBe(200);
     expect(response.header['content-type']).toBe('application/pdf');
   });
+
+  test('responds with a 400 when not given a url or htmlString', async () => {
+    const response = await request(app)
+      .post('/')
+      .send();
+
+    expect(response.status).toBe(400);
+  });
 });
