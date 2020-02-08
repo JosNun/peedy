a84fd4622db7ee9c3485feda93c6e4d24084b98f
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-02-08 14:10:28 -0500

    Simplify tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,24 +1,23 @@
 import request from 'supertest';
 import app from '../app';
 
-describe('Generating a PDF', () => {
-  test('when given a URL, respond with a PDF', async () => {
-    const response = await request(app)
-      .post('/')
-      .send({
-        // TODO: this should be a static site that is in the project
-        url: 'https://example.com',
-      });
+test('when given a URL, respond with a PDF', async () => {
+  const response = await request(app)
+    .post('/')
+    .send({
+      // TODO: this should be a static site that is in the project
+      url: 'https://example.com',
+    });
 
-    expect(response.status).toBe(200);
-    expect(response.header['content-type']).toBe('application/pdf');
-  });
+  expect(response.status).toBe(200);
+  expect(response.header['content-type']).toBe('application/pdf');
+});
 
-  test('responds with a PDF when given an HTML string', async () => {
-    const response = await request(app)
-      .post('/')
-      .send({
-        htmlString: `
+test('responds with a PDF when given an HTML string', async () => {
+  const response = await request(app)
+    .post('/')
+    .send({
+      htmlString: `
           <!DOCTYPE html>
           <html lang="en">
           <head>
@@ -32,17 +31,16 @@ describe('Generating a PDF', () => {
           </body>
           </html>
       `,
-      });
+    });
 
-    expect(response.status).toBe(200);
-    expect(response.header['content-type']).toBe('application/pdf');
-  });
+  expect(response.status).toBe(200);
+  expect(response.header['content-type']).toBe('application/pdf');
+});
 
-  test('responds with a 400 when not given a url or htmlString', async () => {
-    const response = await request(app)
-      .post('/')
-      .send();
+test('responds with a 400 when not given a url or htmlString', async () => {
+  const response = await request(app)
+    .post('/')
+    .send();
 
-    expect(response.status).toBe(400);
-  });
+  expect(response.status).toBe(400);
 });
